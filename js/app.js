f618d402e9d0fda2e2232cf5f7378fdd92f8d859
Claudette Ngai <claudettengai@Claudettes-Air.attlocal.net>
2015-10-05 04:36:21 -0700

    new continuous scrolling design - landing and nav bar -- background-attachment: fixed -- not working

--- js/app.js
+++ js/app.js
@@ -1,3 +1,18 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+
+
+
+
+
+// NAV MENU 
+
+$(function() {
+	$(".right-off-canvas-toggle, .exit-off-canvas").click(function() {
+    	$(".top-bar").toggleClass("top-bar-close");
+		$(".middle-bar").toggleClass("middle-bar-close");
+		$(".bottom-bar").toggleClass("bottom-bar-close");
+  	});
+});
+
