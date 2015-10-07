fd7c127b00a33af5bcdf561691d29e04e83239e5
Claudette Ngai <claudettengai@Claudettes-Air.attlocal.net>
2015-10-07 11:46:39 -0700

    work section overlays responsive jquery -- still unhappy with picture link choices

--- js/app.js
+++ js/app.js
@@ -16,3 +16,71 @@ $(function() {
   	});
 });
 
+
+
+
+
+
+
+
+// WORK SECTION 
+// PROJECT OVERLAYS
+
+
+
+	$('.project-link').mouseenter(function(){
+
+		$(this).find($('.image-overlay-content')).css({
+			width: $(this).find($('.project-image')).width(),
+			height: $(this).find($('.project-image')).height()
+			});
+
+		// $(this).find($('.project-title')).css({
+		// 	marginTop: ($(this).find($('.project-title')).parent().height()/2) - 
+		// 	($(this).find($('.project-title')).height()/2) -40 
+		// });
+
+
+		$(this).find($('.image-overlay-content')).fadeIn(200);
+
+		$(this).find($('.project-title')).animate({
+			marginTop:($(this).find($('.project-title')).parent().height()/2) - 
+			($(this).find($('.project-title')).height()/2)}
+			, 150);
+
+	});
+
+	$('.project-link').mouseleave(function(){
+		$(this).find($('.image-overlay-content')).fadeOut(200);
+
+		$(this).find($('.project-title')).animate({
+			marginTop:($(this).find($('.project-title')).parent().height()/2) - 
+			($(this).find($('.project-title')).height()/2) - 30}
+			, 150);
+
+
+
+	})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
