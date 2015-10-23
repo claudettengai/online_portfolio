f71b70a6b4853f42ea0113bbf4a45d8792bb58c5
Claudette Ngai <claudettengai@Claudettes-Air.hsd1.ca.comcast.net>
2015-10-23 01:52:53 -0700

    project_fe continued

--- js/app.js
+++ js/app.js
@@ -100,4 +100,3 @@ $(document).scroll(placeMenu);
 
 
 
-
---
+++ js/project-fe.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+
+////////hover expand///////////
+
+$('.solutions-detail img').mouseenter(function(){
+
+	$('#hover-expand').fadeIn();
+	
+});
+
+
+$('.solutions-detail img').mouseleave(function(){
+
+	$('#hover-expand').fadeOut();
+	
+});
+
+
+
+
+
+
+
+
+
+
+});
