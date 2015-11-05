4760c6b7e0b421121fbcaa10857e479a52d6723c
Claudette <claudettengai@gmail.com>
2015-11-04 17:54:38 -0800

    FE_replacement working on new format
    working on slanted sub nav bar

--- bower_components/foundation/js/foundation/foundation.magellan.js
+++ bower_components/foundation/js/foundation/foundation.magellan.js
@@ -8,7 +8,7 @@
 
     settings : {
       active_class : 'active',
-      threshold : 0, // pixels from the top of the expedition for it to become fixes
+      threshold : 0;, // pixels from the top of the expedition for it to become fixes
       destination_threshold : 20, // pixels from the top of destination for it to be considered active
       throttle_delay : 30, // calculation throttling to increase framerate
       fixed_top : 0, // top distance in pixels assigend to the fixed element on scroll
@@ -16,6 +16,8 @@
       duration : 700, // animation duration time
       easing : 'swing' // animation easing
     },
+
+    console.log("hello");
 
     init : function (scope, method, options) {
       Foundation.inherit(this, 'throttle');
--- js/project-fe.js
+++ js/project-fe.js
@@ -1,5 +1,55 @@
 $(document).ready(function(){
 
+
+////////////magellan//////////////////////
+
+
+// fixing the jumpiness
+
+
+// console.log(parseInt($('#magellan-bar p').css('margin-top')));
+
+
+// console.log(parseInt($('#magellan-bar p').css('line-height')));
+
+
+
+
+
+
+var magellanPosition = $('#magellan-bar').offset().top + (Math.tan(0.0872665)* $(window).width())  +  (parseInt($('#magellan-bar p').css('line-height')));
+	$window= $(window);
+
+
+var headMarginEqualizer =  parseInt($('.section-title').css('margin-top')) + (0- parseInt($('#magellan-bar').css('margin-top'))) + "px";
+console.log(headMarginEqualizer);
+
+
+
+
+
+$window.scroll(function(){
+	if ($(window).scrollTop() >= magellanPosition) {
+		$("div#magellan-bar").addClass("fixed");
+		// $("div#magellan-bar").css({
+		// 	"background-color": "rgb(0%, 0%, 0%, 0.75)",
+		// 	"color": "white",
+
+		// })
+
+
+		$("h2.section-title").css("margin-top", headMarginEqualizer);
+
+
+
+	} else {
+		$("div#magellan-bar").removeClass("fixed");
+		
+	}
+});
+
+
+
 ////////hover expand///////////
 
 $('.solutions-detail img').mouseenter(function(){
@@ -19,9 +69,7 @@ $('.solutions-detail img').mouseleave(function(){
 
 
 
-// $.get("nav.html", function(data){
-//     $(this).children.html(data);
-// });
+
 
 
 });
