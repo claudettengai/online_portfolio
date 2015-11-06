a9e18171997c097a143e185f6d728cbc7d5fc09d
Claudette <claudettengai@gmail.com>
2015-11-05 20:38:14 -0800

    working magellan-- fixed for slant weirdness and for initial margin issue

--- js/project-fe.js
+++ js/project-fe.js
@@ -13,40 +13,66 @@ $(document).ready(function(){
 // console.log(parseInt($('#magellan-bar p').css('line-height')));
 
 
+// $('#magellan-bar').css({"margin-top": - ($(window).height() * 0.08)});
+// console.log('hello');
+
+var heroBottom = $('#FE-head-back').position().top+$('#FE').outerHeight(true)
 
 
-
-
-var magellanPosition = $('#magellan-bar').offset().top + (Math.tan(0.0872665)* $(window).width())  +  (parseInt($('#magellan-bar p').css('line-height')));
+var magellanPositionScroll = $('#magellan-bar').offset().top + (Math.tan(0.0698132)* $(window).width()) + 16;
 	$window= $(window);
 
-
-var headMarginEqualizer =  parseInt($('.section-title').css('margin-top')) + (0- parseInt($('#magellan-bar').css('margin-top'))) + "px";
-console.log(headMarginEqualizer);
+console.log($('#magellan-bar').offset().top);
 
 
+// var headMarginEqualizer =  parseInt($('.after-fixed-nav').css('margin-top')) + (0- parseInt($('#magellan-bar').css('margin-top'))) + "px";
+// console.log(headMarginEqualizer);
 
+var headMarginInit = $("h2.after-fixed-nav").css("margin-top");
 
+var headMarginEqualizer = parseInt($("div#magellan-bar").outerHeight(true))+ parseInt($("h2.after-fixed-nav").css("margin-top")) + "px";
+// console.log(headMarginEqualizer);
 
 $window.scroll(function(){
-	if ($(window).scrollTop() >= magellanPosition) {
-		$("div#magellan-bar").addClass("fixed");
+	if ($(window).scrollTop() >= magellanPositionScroll) {
+		$("#magellan-bar").addClass("fixed");
 		// $("div#magellan-bar").css({
-		// 	"background-color": "rgb(0%, 0%, 0%, 0.75)",
-		// 	"color": "white",
+		// 	"background-color": "rgba(0, 0, 0, 0.8)",
+		// });
 
-		// })
-
-
-		$("h2.section-title").css("margin-top", headMarginEqualizer);
+		$("h2.after-fixed-nav").css("margin-top", headMarginEqualizer);
 
 
 
 	} else {
 		$("div#magellan-bar").removeClass("fixed");
+		$("h2.after-fixed-nav").css("margin-top", headMarginInit);
+
 		
 	}
 });
+
+
+// $(document).foundation({
+// "magellan-expedition": {
+  
+//   threshold: 400, // how many pixels until the magellan bar sticks, 0 = auto
+//   offset_by_height : false,  // whether to offset the destination by the expedition height. Usually you want this to be true, unless your expedition is on the side.
+//    destination_threshold : 400, // pixels from the top of destination for it to be considered active
+//    fixed_top : 0, // top distance in pixels assigend to the fixed element on scroll
+
+// }
+// });
+
+console.log("hello");
+
+
+
+
+
+
+
+
 
 
 
