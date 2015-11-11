3d3140be0e23b2ec09106b468e5e5324ce0a63df
Claudette <claudettengai@gmail.com>
2015-11-10 17:40:49 -0800

    FE_replacement.html contains all of the content Main Styles/ nav pretty much finished
    organization -- 1. background/process 2.user research 3. strategy 4. broad solutions 5. specific changes 6. development

--- js/project-fe.js
+++ js/project-fe.js
@@ -1,31 +1,55 @@
 $(document).ready(function(){
 
 
+
+
+// degrees to radians function	
+
+Math.degrees = function(rad)
+ {
+ return rad*(180/Math.PI);
+ }
+ 
+Math.radians = function(deg)
+ {
+ return deg * (Math.PI/180);
+ }
+
+// var angle_in_deg = Math.degrees(3.14);
+// var angle_in_rad = Math.radians(180);
+
+
+
+
+
+
 ////////////magellan//////////////////////
 
 $(document).foundation({
 "magellan-expedition": {
  
-  destination_threshold: 50, // pixels from the top of destination for it to be considered active
+  destination_threshold: 70, // pixels from the top of destination for it to be considered active
    
 }
 });
+
+
+
+
+
+
 // fixing the jumpiness
 
 
 // console.log(parseInt($('#magellan-bar p').css('margin-top')));
-
-
 // console.log(parseInt($('#magellan-bar p').css('line-height')));
-
-
 // $('#magellan-bar').css({"margin-top": - ($(window).height() * 0.08)});
 // console.log('hello');
 
 var heroBottom = $('#FE-head-back').position().top+$('#FE').outerHeight(true)
 
 
-var magellanPositionScroll = $('#magellan-bar').offset().top + (Math.tan(0.0698132)* $(window).width()) + 16;
+var magellanPositionScroll = $('#magellan-bar').offset().top + (Math.tan(Math.radians(4))* $(window).width()) + 16;
 	$window= $(window);
 
 console.log($('#magellan-bar').offset().top);
@@ -68,20 +92,29 @@ $window.scroll(function(){
 
 
 
-function skewAdjust(){
+// function skewAdjust(){
 
-	var imgSkewAdjust = $('#fe-pic1').offset().top- $('.image-clip').offset().top;
-	$(".image-clip").css("margin-top", -imgSkewAdjust);
-};
-
-
-skewAdjust();
-
-$(window).resize(function(){
-	console.log(imgSkewAdjust);
-});
+// 	var imgSkewAdjust = $('#fe-pic1').offset().top- $('.image-clip').offset().top;
+// 	$(".image-clip").css("margin-top", -imgSkewAdjust);
+// };
 
 
+// skewAdjust();
+
+// $(window).resize(function(){
+// 	console.log(imgSkewAdjust);
+// });
+
+
+
+function skewTriangles(){
+	var triangleTopHeight = Math.tan(Math.radians(4)) * $(window).width();
+	$('.image-clip-before, .image-clip-after').css("height", triangleTopHeight);
+}
+
+skewTriangles();
+
+$(window).resize(skewTriangles);
 
 
 
